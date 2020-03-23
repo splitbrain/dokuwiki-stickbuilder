82a4df7e9db224d6a0690c8d2dc24129270a82ac
Andreas Gohr <andi@splitbrain.org>
2020-03-23 22:50:42 +0100

    Automatically copy the needed dependencies
    The script now automatically figures out which DLLs are needed for the
    binaries and copies them over automatically.

--- build.sh
+++ build.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
 
 APACHE_ZIP="https://www.apachelounge.com/download/VS16/binaries/httpd-2.4.41-win32-VS16.zip"
-PHP_ZIP="http://windows.php.net/downloads/releases/php-7.3.16-Win32-VC15-x86.zip"
+PHP_ZIP="https://windows.php.net/downloads/releases/php-7.3.16-Win32-VC15-x86.zip"
+VC_EXE="https://aka.ms/vs/16/release/VC_redist.x86.exe"
+
+APACHE_MODULES="access_compat authz_core dir env log_config mime rewrite setenvif"
+PHP_EXTENSIONS="bz2 gd2 ldap mbstring opcache openssl pdo_sqlite"
+
+# Try to identify the DLLs that the given binary needs and copy them from
+# the VC redistributable and the binary's own folder. Check dpendencies of
+# the found DLLs recursively
+function copyruntimelibs() {
+    local SRC   # the binary to check
+    local SELF  # path of the binary
+    local PSELF # parent path of the binary
+    local DLL   # a found dependency
+    local LC    # the dependency lowercased
+    local LLC   # the dpendency with underscores instead of hyphens
+    local ORG   # path to the matching DLL file
+
+    SRC=$1
+    SELF=$(dirname "$SRC")
+
+    for DLL in $(objdump --private-headers "$SRC" |grep 'DLL Name'|awk '{print $3}'); do
+        LC=$(echo "$DLL" | tr '[:upper:]' '[:lower:]')
+        LLC=$(echo "$LC" | tr '-' '_')
+        PSELF="$SELF/.."
+
+        for ORG in "$SELF/$DLL" "$PSELF/$DLL" "tmp/vc/$LC" "tmp/vc/$LLC"; do
+            if [ -e "$ORG" ]; then
+                if [ ! -e "out/server/$DLL" ]; then
+                    # copy the DLL
+                    cp -v "$ORG" "out/server/$DLL"
+                    # check dependencies for the DLL
+                    copyruntimelibs "$ORG"
+                fi
+            fi
+        done
+    done
+}
 
 # an up-to-date upx is preferable now that development has started again
 if [ ! -e "./upx" ]; then
@@ -9,6 +46,17 @@ if [ ! -e "./upx" ]; then
     echo "See https://github.com/upx/upx/releases/"
     exit
 fi
+
+if ! command -v cabextract >/dev/null 2>&1; then
+    echo "Please install the cabextract utility"
+    exit
+fi
+
+if ! command -v objdump >/dev/null 2>&1; then
+    echo "Please install the objdump utility"
+    exit
+fi
+
 
 # we use previously downloaded files, but we warn about it
 if [ -d "tmp" ]; then
@@ -18,7 +66,7 @@ fi
 
 # clean up
 if [ -d "out" ]; then
-    rm -rf out
+    rm -rf "out"
 fi
 
 # create folders
@@ -32,19 +80,29 @@ mkdir -p out/server/php/ext
 # copy default configs
 cp -r tpl/* out/
 
-# download and unpack Apache and PHP
+# download and unpack Apache, PHP and the Visual Studio Redistributable
 if [ ! -f "tmp/apache.zip" ]; then
     wget --user-agent="" "$APACHE_ZIP" -O tmp/apache.zip
 fi
 if [ ! -f "tmp/php.zip" ]; then
     wget "$PHP_ZIP" -O tmp/php.zip
 fi
+if [ ! -f "tmp/vc.exe" ]; then
+    wget "$VC_EXE" -O tmp/vc.exe
+fi
+
+# delete old unpacks
 if [ -d "tmp/apache" ]; then
     rm -rf tmp/apache
 fi
 if [ -d "tmp/php" ]; then
     rm -rf tmp/php
 fi
+if [ -d "tmp/vc" ]; then
+    rm -rf tmp/vc
+fi
+
+# unpack sources
 cd tmp || exit
     unzip apache.zip
     mv Apache[0-9][0-9] apache
@@ -55,37 +113,31 @@ cd tmp || exit
     cd php || exit
     unzip ../php.zip
     cd ..
+
+    mkdir vc
+    cd vc || exit
+    cabextract -L ../vc.exe
+    cabextract -L *
+    cd ..
 cd ..
 
-# copy stuff
+# copy Apache
 cp tmp/apache/LICENSE.txt                   out/server/apache-license.txt
 cp tmp/apache/bin/httpd.exe                 out/server/mapache.exe
-cp tmp/apache/bin/libapr-1.dll              out/server/
-cp tmp/apache/bin/libapriconv-1.dll         out/server/
-cp tmp/apache/bin/libaprutil-1.dll          out/server/
-cp tmp/apache/bin/libhttpd.dll              out/server/
-cp tmp/apache/bin/pcre.dll                  out/server/
-cp tmp/apache/modules/mod_access_compat.so  out/server/modules/
-cp tmp/apache/modules/mod_authz_core.so     out/server/modules/
-cp tmp/apache/modules/mod_dir.so            out/server/modules/
-cp tmp/apache/modules/mod_env.so            out/server/modules/
-cp tmp/apache/modules/mod_log_config.so     out/server/modules/
-cp tmp/apache/modules/mod_mime.so           out/server/modules/
-cp tmp/apache/modules/mod_rewrite.so        out/server/modules/
-cp tmp/apache/modules/mod_setenvif.so       out/server/modules/
+copyruntimelibs tmp/apache/bin/httpd.exe
+for MOD in $APACHE_MODULES; do
+    cp "tmp/apache/modules/mod_$MOD.so" "out/server/modules/"
+    copyruntimelibs "tmp/apache/modules/mod_$MOD.so"
+done
 
+# copy PHP
 cp tmp/php/license.txt                      out/server/php/php-license.txt
-cp tmp/php/libeay32.dll                     out/server/php/
-cp tmp/php/php5apache2_4.dll                out/server/php/
-cp tmp/php/php5ts.dll                       out/server/php/
-cp tmp/php/ssleay32.dll                     out/server/php/
-cp tmp/php/ext/php_bz2.dll                  out/server/php/ext/
-cp tmp/php/ext/php_gd2.dll                  out/server/php/ext/
-cp tmp/php/ext/php_ldap.dll                 out/server/php/ext/
-cp tmp/php/ext/php_mbstring.dll             out/server/php/ext/
-cp tmp/php/ext/php_opcache.dll              out/server/php/ext/
-cp tmp/php/ext/php_openssl.dll              out/server/php/ext/
-cp tmp/php/ext/php_pdo_sqlite.dll           out/server/php/ext/
+cp tmp/php/php7apache2_4.dll                out/server/php/
+copyruntimelibs tmp/php/php7apache2_4.dll
+for EXT in $PHP_EXTENSIONS; do
+    cp "tmp/php/ext/php_$EXT.dll" "out/server/php/ext/"
+    copyruntimelibs "tmp/php/ext/php_$EXT.dll"
+done
 
 # compress files
 ./upx out/server/*.dll
